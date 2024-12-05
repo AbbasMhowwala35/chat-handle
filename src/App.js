da257b0d4ffcefc29d14c30211cc379ae46b927b
Abbas Mhowwala <abbas@mobiiworld.com>
2024-12-05 17:17:58 +0530

    Git Chat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HomePage from './components/HomePage';
+import './theme.css'
+import LanguagePopup from './components/common/LanguagePopup';
 function App() {
+  const [language, setLanguage] = useState('English');
+  const handleLanguageSelect = (selectedLanguage) => {
+    setLanguage(selectedLanguage);
+    console.log(`Selected language: ${selectedLanguage}`);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex">
+      {/* <LanguagePopup onSelectLanguage={handleLanguageSelect} /> */}
+      {/* <div id="google_translate_element" style={{ display: 'none' }}></div> */}
+      <HomePage />
     </div>
   );
 }
