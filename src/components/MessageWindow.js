57a73127462f6e4fd8592a9145a4a05d8078bdf2
Abbas Mhowwala <abbas@mobiiworld.com>
2024-12-07 13:06:26 +0530

    API FIxes

--- src/components/MessageWindow.js
+++ src/components/MessageWindow.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Badge, Carousel, Form } from 'react-bootstrap';
 import img1 from '../images/2.jpeg';
 import img2 from '../images/ai.jpg';
+
 const MessageWindow = () => {
   const [messages, setMessages] = useState([
     { id: 1, text: 'Hi there! I’m Mr. Lynx, your AI assistant. Let me help you find the perfect expert! Simply describe the person you’re searching for, and I’ll connect you with the best fit.', sender: 'bot' },
@@ -10,6 +11,7 @@ const MessageWindow = () => {
   const [attachment, setAttachment] = useState(null);
   const [filteredProfiles, setFilteredProfiles] = useState([]);
   const [threadId, setThreadId] = useState(null);
+
   const handleSendMessage = async () => {
     if (newMessage.trim() || attachment) {
       const userMessage = {
@@ -47,6 +49,26 @@ const MessageWindow = () => {
               avatar: profile[4] || 'https://via.placeholder.com/50',
             }));
             setFilteredProfiles(parsedProfiles);
+            // Add profiles to messages as a new bot message
+            setMessages((prev) => [
+              ...prev,
+              {
+                id: prev.length + 1,
+                text: 'Here are some relevant profiles:',
+                sender: 'bot',
+              },
+            ]);
+            setMessages((prev) => [
+              ...prev,
+              ...parsedProfiles.map((profile) => ({
+                id: prev.length + 1,
+                text: `${profile.name}, ${profile.profession}, ${profile.location}. 
+                <a href="${profile.avatar}" target="_blank" rel="noopener noreferrer" style="font-size: 0.9rem; color: #007bff; text-decoration: underline;">
+                  View Profile
+                </a>`,
+                sender: 'bot',
+              })),
+            ]);
           }
         } else {
           console.error('Failed to fetch data from the API');
@@ -64,6 +86,27 @@ const MessageWindow = () => {
       setAttachment(event.target.files[0]);
     }
   };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSendMessage();
+    }
+  };
+
+  const handleSearch = (searchText) => {
+    setNewMessage(''); // Clear message box on search
+    // Handle your search logic here (if applicable)
+    // Append a message for search (if needed)
+    setMessages((prev) => [
+      ...prev,
+      {
+        id: prev.length + 1,
+        text: `Searching for: ${searchText}`,
+        sender: 'user',
+      },
+    ]);
+  };
+
   useEffect(() => {
     const fetchThreadId = async () => {
       try {
@@ -72,7 +115,7 @@ const MessageWindow = () => {
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({}), 
+          body: JSON.stringify({}),
         });
         if (!startResponse.ok) {
           console.error('Failed to fetch thread ID');
@@ -84,7 +127,7 @@ const MessageWindow = () => {
         console.error('Error fetching thread ID:', error);
       }
     };
-    fetchThreadId(); 
+    fetchThreadId();
   }, []);
 
   return (
@@ -131,7 +174,12 @@ const MessageWindow = () => {
               className={`message-container ${message.sender === 'bot' ? 'bot' : 'user'}`}
             >
               <div className={`message-bubble ${message.sender === 'bot' ? 'bot' : 'user'}`}>
-                {message.text}
+              <div
+                  key={index}
+                  className={`message-container ${message.sender === 'bot' ? 'bot' : 'user'} d-block`}
+                  dangerouslySetInnerHTML={{ __html: message.text }}
+                />
+
                 {message.attachment && (
                   <div className="attachment-preview">
                     <strong>Attached: </strong> {message.attachment.name}
@@ -140,32 +188,29 @@ const MessageWindow = () => {
               </div>
             </div>
           ))}
-          <div className="search-results">
-            {filteredProfiles.length > 0 &&
-              filteredProfiles.map((profile) => (
-                <div
-                  key={profile.id}
-                  className="profile-item d-flex align-items-center mb-2 bg-light p-3 m-3"
-                >
-                  <div>
-                    <h6 className="mb-0">{profile.name}</h6>
-                    <p className="mb-0">
-                      {profile.profession}, {profile.location}
-                    </p>
-                    <a
-                      href={profile.avatar}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      style={{ fontSize: '0.9rem', color: '#007bff', textDecoration: 'underline' }}
-                    >
-                      View Profile
-                    </a>
-                  </div>
+          {/* {filteredProfiles.length > 0 &&
+            filteredProfiles.map((profile) => (
+              <div
+                key={profile.id}
+                className="profile-item d-flex align-items-center mb-2 bg-light p-3 m-3"
+              >
+                <div>
+                  <h6 className="mb-0">{profile.name}</h6>
+                  <p className="mb-0">
+                    {profile.profession}, {profile.location}
+                  </p>
+                  <a
+                    href={profile.avatar}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ fontSize: '0.9rem', color: '#007bff', textDecoration: 'underline' }}
+                  >
+                    View Profile
+                  </a>
                 </div>
-              ))}
-          </div>
+              </div>
+            ))} */}
         </div>
-
         <div className="message-input">
           <label htmlFor="attachment" className="attachment-icon">
             <svg
@@ -176,24 +221,19 @@ const MessageWindow = () => {
               fill="none"
             >
               <path
-                d="M3.24694 14.9763C2.38586 14.9763 1.55993 14.6194 0.95105 13.984C0.342174 13.3487 0.000131465 12.487 3.61037e-08 11.5885C-0.000128304 10.6899 0.341911 9.8281 0.950689 9.19261L8.41287 1.40598C9.57493 0.193401 11.2688 -0.280188 12.8561 0.163675C14.4437 0.607504 15.6835 1.90141 16.109 3.55782C16.5343 5.21437 16.0805 6.9817 14.9183 8.19428L12.0501 11.1872C11.9492 11.2961 11.8108 11.3582 11.6656 11.3595C11.5204 11.3609 11.3808 11.3013 11.2782 11.1941C11.1756 11.087 11.1185 10.9414 11.1197 10.79C11.121 10.6385 11.1804 10.4938 11.2849 10.3887L14.1531 7.3958V7.39567C15.0418 6.46834 15.3887 5.11687 15.0634 3.85019C14.7381 2.5835 13.7899 1.59401 12.5758 1.25479C11.3619 0.915417 10.0235 1.23362 9.23151 2.26213L2.76935 10.0489C2.67045 10.1587 2.50355 10.2166 2.34446 10.2166C2.18537 10.2166 2.01847 10.1587 1.91957 10.0489C1.82067 9.93902 1.76283 9.77265 1.76283 9.60399C1.76283 9.43534 1.82067 9.26897 1.91957 9.15908L7.04214 3.0919C7.60443 2.6028 8.47769 2.6028 9.04085 3.0919L13.3325 6.94161C13.8958 7.42987 14.039 8.37406 13.5508 8.93734L10.6947 11.8451C10.2067 12.3997 9.30189 12.4158 8.74538 11.9279L3.24694 14.9763Z"
-                fill="#212121"
-              />
+                d="M3.24694 14.9763C2.38586 14.9763 1.55993 14.6194 0.95105 13.984C0.342174 13.3487 0.000131465 12.487 3.61037e-08 11.5885C-0.000128304 10.6899 0.341911 9.8281 0.950689 9.19261L8.41287 1.40598C9.57493 0.193401 11.2688 -0.280188 12.8561 0.163675C14.4437 0.607504 15.6835 1.90141 16.109 3.55782C16.5343 5.21437 16.0805 6.9817 14.9183 8.19428L12.0501 11.1872C11.9492 11.2961 11.8108 11.3582 11.6656 11.3595C11.5204 11.3609 11.3808 11.3013 11.2782 11.1941C11.1756 11.087 11.1185 10.9414 11.1197 10.79C11.121 10.6385 11.1804 10.4938 11.2849 10.3887L14.1531 7.3958V7.39567C15.0418 6.46834 15.3887 5.11687 15.0634 3.85019C14.7381 2.5835 13.7899 1.59401 12.5758 1.25479C11.3619 0.915417 10.0235 1.23362 9.23151 2.26213L2.76935 10.0489C2.67045 10.1587 2.50355 10.2166 2.34446 10.2166C2.18537 10.2166 2.01847 10.1587 1.91957 10.0489C1.82067 9.9391 1.82067 9.78095 1.91957 9.67114L9.38175 1.88448C9.91773 1.26149 11.1874 1.22256 11.7482 1.81077C12.3092 2.39898 12.3092 3.3681 11.7482 3.9576L4.38046 11.7445C3.97399 12.2718 3.38193 12.4955 2.78123 12.4876L3.24694 14.9763Z"
+                fill="#000"
+              ></path>
             </svg>
           </label>
           <input
-            type="file"
-            id="attachment"
-            onChange={handleAttachmentChange}
-            style={{ display: 'none' }}
-          />
-          <input
             type="text"
-            placeholder="Type a message"
+            className="form-control"
             value={newMessage}
             onChange={(e) => setNewMessage(e.target.value)}
+            onKeyDown={handleKeyPress}
           />
-          <button className="send-button" onClick={handleSendMessage}>
+          <button className="btn btn-primary" onClick={handleSendMessage}>
             Send
           </button>
         </div>
